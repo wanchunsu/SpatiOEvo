82ac3939b1220fbe7cb8fedf704fe5051cb3ca1d
wanchunsu <37888451+wanchunsu@users.noreply.github.com>
2026-04-09 20:02:40 -0400

    Update instructions.bash

--- scripts/instructions.bash
+++ scripts/instructions.bash
@@ -9,7 +9,7 @@ python3 construct_sub_int_cg.py
 # Step 3: Construct whole interface contact graph, label nodes and edges
 python3 construct_whole_interface_cg.py 
 
-# Step 4: Compute O/E contact count ratios for different interfaces and sub-interfaces, based on 10,000 intra-protein node-label randomization trials) 
+# Step 4: Compute O/E contact count ratios for different interfaces and sub-interfaces, based on 10,000 intra-protein node-label randomization trials 
 python3 oe_diff_interfaces.py 
 
 # Step 5: Compute O/E pos-pos contact count ratios between distinct interfaces, based on 10,000 intra-protein node-label randomization trials (where individual interfaces are shuffled separately) 
